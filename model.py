a49e05b6bf3552a4d789b71e02196d36dd5c0d16
Eklavya Jain <eklavya1012@gmail.com>
2021-01-15 23:51:45 +0530

    update

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # import keras
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 from tensorflow.keras.models import model_from_json
 import cv2 as cv2
 
